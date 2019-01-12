f6404d4c81b42174844c4b39c73414ed1e67d3a1
sanjusss <sanjusss@qq.com>
2019-01-12 15:54:58 +0800

    删除多余的推送设置。

--- deploy.ps1
+++ deploy.ps1
@@ -27,7 +27,5 @@ if ($env:ARCH -eq "amd64") {
         "sanjusss/dotnet-helloworldwithdocker:arm32v7" `
         "sanjusss/dotnet-helloworldwithdocker:arm64v8" `
         "sanjusss/dotnet-helloworldwithdocker:amd64"
-    docker manifest annotate "sanjusss/dotnet-helloworldwithdocker:latest" "sanjusss/dotnet-helloworldwithdocker:arm32v7" --os linux --arch arm --variant v7
-    docker manifest annotate "sanjusss/dotnet-helloworldwithdocker:latest" "sanjusss/dotnet-helloworldwithdocker:arm64v8:APPVEYOR_REPO_TAG_NAME" --os linux --arch arm64 --variant v8
     docker manifest push "sanjusss/dotnet-helloworldwithdocker:latest"
 }
